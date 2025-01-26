95a18f1a61b3de05a39a64bcb34c877426309abd
Ian Andersen Ng <ianandersen@192.168.1.4>
2025-01-26 15:08:35 +0800

    Add cars and getAllCars

--- CarRental.sol
+++ CarRental.sol
@@ -6,18 +6,23 @@ contract CarRental {
    struct Car {
     string carPlate;
     bool isDamaged;
+    bool isRented;
     uint256 deposit;
     address renterAddress;
    }
 
-    // for car rental
+    // Global Variables
     address private immutable owner;
     mapping(string => Car) private carMap;
+    string[] private carPlates;
 
-    // for ICO
     ERC20 public token;
     address payable public wallet;
     uint256 public amountRaised;
+
+    // Events for logging
+    event CarAdded(string carPlate, address renterAddress);
+    event CarDetails(string carPlate, bool isDamaged, bool isRented, uint256 deposit, address renterAddress);
 
     constructor(ERC20 _token) {
         owner = msg.sender;
@@ -25,6 +30,13 @@ contract CarRental {
         token = _token; 
     }
 
+    // modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the car rental owner can do this.");
+        _;
+    }
+
+    // methods
     function buyTokens() public payable {
         uint256 numTokens = msg.value / 1 ether;
         amountRaised += msg.value;
@@ -32,6 +44,32 @@ contract CarRental {
         wallet.transfer(msg.value);
     }
 
-    // will make more to add car into the car map
+    function addCar(
+        string memory carPlate,
+        uint256 deposit,
+        address renterAddress
+    ) external onlyOwner {
+        Car storage newCar = carMap[carPlate];
+        newCar.carPlate = carPlate;
+        newCar.deposit = deposit;
+        newCar.isRented = false;
+        newCar.isDamaged = false;
+        newCar.renterAddress = renterAddress;    
+        carPlates.push(carPlate);
+        emit CarAdded(carPlate, renterAddress);
+    }
+
+    function getAllCars() external onlyOwner {
+        for (uint256 i = 0; i < carPlates.length; i++) {
+            Car storage car = carMap[carPlates[i]];
+            emit CarDetails(
+                car.carPlate, 
+                car.isDamaged, 
+                car.isRented, 
+                car.deposit, 
+                car.renterAddress
+            );
+        } 
+    }
 
 }
